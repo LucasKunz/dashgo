c1a96615cca96863960a227a1cc24ac5a4ef74f5
Lucas Kunz <lucaskunz1@gmail.com>
2022-03-20 18:24:59 -0300

    Create pagination function

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@chakra-ui/react";
 
 interface PaginationItemProps {
-  number: string;
+  number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void;
 }
 
 export default function PaginationItem({
   isCurrent = false,
   number,
+  onPageChange
+  
 }: PaginationItemProps) {
   if (isCurrent) {
     return (
@@ -24,7 +27,7 @@ export default function PaginationItem({
     );
   }
   return (
-    <Button size="sm" fontSize="xs" width="4" _hover={{ bg: "gray.500" }}>
+    <Button size="sm" fontSize="xs" width="4" bg='gray.700' _hover={{ bg: "gray.500" }} onClick={() => onPageChange(number)}>
       {number}
     </Button>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,30 +21,12 @@ import Header from "../../components/Header";
 import Sidebar from "../../components/SideBar";
 import Pagination from "../../components/Pagination";
 import Link from "next/link";
-import { useQuery } from "react-query";
+import { useUsers } from "../../services/hooks/useUser";
+import { useState } from "react";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users;
-  }, {
-    staleTime: 1000 * 5
-  });
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -61,6 +43,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href={"/users/create"} passHref>
               <Button
@@ -97,7 +82,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -131,7 +116,7 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination totalCountOfRegisters={data.totalCount} currentPage={page} onPageChange={setPage} />
             </>
           )}
         </Box>
